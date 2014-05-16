ba22b5140e6e03919337799cdfa245f3a44304ad
Matt Fenwick <mfenwick100@gmail.com>
2014-05-15 20:09:57 -0400

    more queries for analyzing

--- Contents/Resources/python/sparky/r_model.py
+++ Contents/Resources/python/sparky/r_model.py
@@ -25,31 +25,6 @@ def resonances():
         rs.extend(c.resonance_list())
     return rs
 
-def resonances_from_peaks():
-    """
-    expected to match resonances ... but is it true ??
-    """
-    my_rs = set([])
-    for spectrum in project().spectrum_list():
-        for pk in spectrum.peak_list():
-            for r in pk.resonances():
-                if r is not None:
-                    my_rs.add(r)
-    return my_rs
-
-def resonance_map():
-    gs = {}
-    for r in resonances():
-        gid, _, _ = parse_group(r.group.name)
-        if not gs.has_key(gid):
-            gs[gid] = {}
-        rid, _ = parse_resonance(r.atom.name)
-        gs[gid][rid] = r
-    return gs
-
-def group_map():
-    return dict((g.name, g) for g in groups())
-
 
 #### group and resonances names from my model
 
@@ -77,13 +52,56 @@ def unparse_resonance(rid, atom):
     return '%s[%s]' % (rid, atom)
 
 
+#### some more complex data structures, built from the Sparky model
+
+def _from_peaks():
+    """
+    expected to match resonances ... but is it true ??
+    """
+    my_rs, my_gs = set([]), set([])
+    for spectrum in project().spectrum_list():
+        for pk in spectrum.peak_list():
+            for r in pk.resonances():
+                if r is not None:
+                    my_rs.add(r)
+                    my_gs.add(r.group)
+    return (my_rs, my_gs)
+
+def resonances_from_peaks():
+    rs, _ = _from_peaks()
+    return rs
+
+def groups_from_peaks():
+    _, gs = _from_peaks()
+    return gs
+
+def resonance_map():
+    gs, bad = {}, {}
+    for r in resonances_from_peaks():
+        try:
+            gid, _, _ = parse_group(r.group.name)
+            if not gs.has_key(gid):
+                gs[gid] = {}
+            rid, _ = parse_resonance(r.atom.name)
+            gs[gid][rid] = r
+        except:
+            if not bad.has_key(r.group.name):
+                bad[r.group.name] = {}
+            bad[r.group.name][r.atom.name] = r
+    return gs, bad
+
+def group_map():
+    return dict((g.name, g) for g in groups())
+
+
 #### assignment utilities
 
 def _add_into_cluster(cls, val):
     n = 0
     for c in cls:
         v = c[0]
-        if abs((v - val) / (1.0 * val)) < 0.01:
+        diff = abs((v - val) / (1.0 * val))
+        if diff < 0.002:
             return c[1]
         n = c[1]
     cls.append((val, n + 1))
@@ -94,10 +112,15 @@ def _selected_peaks():
 
 def set_group(name):
     pks = _selected_peaks()
-    r_ids = []
+    groups, _ = resonance_map()
+    if groups.has_key(name):
+        r_ids = sorted([(res.frequency, int(ix)) for (ix, res) in groups[name].items()], key=lambda x: x[1])
+    else:
+        r_ids = []
     n = 0
     for pk in pks:
         for i in range(len(pk.frequency)):
+            # TODO should this iteration be skipped if peak dim is already assigned a resonance?
             n += 1
             my_id = _add_into_cluster(r_ids, pk.frequency[i])
             pk.assign(i, 
@@ -107,11 +130,26 @@ def set_group(name):
     print n, 'assignments made'
 
 
+def set_new_group():
+    groups, _ = resonance_map()
+    n = len(groups) + 1
+    while groups.has_key(str(n)):
+        n = n + 1
+    set_group(str(n))
+
+
 def set_next_ss(prev, next_):
     raise ValueError('unimplemented')
 
 
-def set_residue(ss, residue):
+def set_residue(ssname, residue):
+    groups, _ = resonance_map()
+    if not groups.has_key(ssname):
+        raise ValueError('invalid group name: %s' % ssname)
+    # for every resonance in this group
+    # for every peak in the resonance's peak_list
+    # for every peak dimension that's assigned to this group ???
+    # do something?
     raise ValueError('unimplemented')
 
 
@@ -126,6 +164,7 @@ def set_noise():
         if set(pk.resonances()) != set([None]):
             raise ValueError("peak dimension is assigned: cannot set to noise %s" % str(pk))
         pk.color = 'red'
+        pk.note = 'noise'
         # pk.show_label('noise')
 
 
@@ -135,6 +174,7 @@ def set_artifact():
         if set(pk.resonances()) != set([None]):
             raise ValueError("peak dimension is assigned: cannot set to artifact %s" % str(pk))
         pk.color = 'blue'
+        pk.note = 'artifact'
         # pk.show_label('artifact')
 
 
