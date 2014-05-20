08832c014c5b922d31f3064da2a041c36c9a6c64
Matt Fenwick <mfenwick100@gmail.com>
2014-05-20 16:14:17 -0400

    add ambiguous peaktypes to lists

--- Contents/Resources/python/sparky/r_peaktypes.py
+++ Contents/Resources/python/sparky/r_peaktypes.py
@@ -11,29 +11,36 @@ nhsqc = [
     ['HE'  , 'NE'   ],
     ['HD21', 'ND2'  ],
     ['HD22', 'ND2'  ],
+    ['HD2[12]', 'ND2']
     ['HE21', 'NE2'  ],
     ['HE22', 'NE2'  ],
+    ['HE2[12]', 'NE2']
+    ['H[DE][12]', 'N[DE]2']
     ['HE1' , 'NE1'  ]
 ]
 
-# how should ambiguity and sidechains be handled?
-# e.g. 45[?]{C12}(1)    <-- is_sidechain in parens in group name?
-#      7[HD2(1)]-8[ND2]-9[CB]    <-- ambiguity in parens in atomname?
-#      4[H]-5[N]-6[CA(i-1)]
 hncacb = [
     ['H'    , 'N'  , 'CA'       ],
     ['H'    , 'N'  , 'CA(i-1)'  ],
+    ['H'    , 'N'  , 'CA(i/i-1)'],
     ['H'    , 'N'  , 'CB'       ],
     ['H'    , 'N'  , 'CB(i-1)'  ],
+    ['H'    , 'N'  , 'CB(i/i-1)'],
     ['HE'   , 'NE' , 'CD'       ],
     ['HD21' , 'ND2', 'CB'       ],
-    ['HD21' , 'ND2', 'CA'       ],
     ['HD22' , 'ND2', 'CB'       ],
+    ['HD21' , 'ND2', 'CA'       ],
     ['HD22' , 'ND2', 'CA'       ],
+    ['HD2[12]', 'ND2', 'CB'     ],
+    ['HD2[12]', 'ND2', 'CA'     ],
     ['HE21' , 'NE2', 'CG'       ],
-    ['HE21' , 'NE2', 'CB'       ],
     ['HE22' , 'NE2', 'CG'       ],
-    ['HE22' , 'NE2', 'CB'       ]
+    ['HE21' , 'NE2', 'CB'       ],
+    ['HE22' , 'NE2', 'CB'       ],
+    ['HE[12]', 'NE2', 'CG'      ],
+    ['HE[12]', 'NE2', 'CB'      ],
+    ['H[DE][12]', 'N[DE]2', 'CG'],
+    ['H[DE][12]', 'N[DE]2', 'CB']
 ]
 
 hn_co_cacb = [
