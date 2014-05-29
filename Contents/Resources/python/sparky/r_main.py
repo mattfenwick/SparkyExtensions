f6b389280a69345ce201207382d15efb44b59ce7
Matt Fenwick <mfenwick100@gmail.com>
2014-05-29 07:18:07 -0400

    start dialog for peak-GSS assignment

--- Contents/Resources/python/sparky/r_main.py
+++ Contents/Resources/python/sparky/r_main.py
@@ -47,6 +47,57 @@ import simplejson as json
 
 
 
+class Peak_grouper_dialog(tkutil.Dialog):
+
+    def __init__(self, session):
+        self.session = session
+        self.title = 'Assign peaks into GSSs'
+        
+        tkutil.Dialog.__init__(self, session.tk, self.title)
+
+        _spectrum_names = [sp.name for sp in model.spectra()]
+        self.spec_from = m1 = tkutil.option_menu(self.top, 'Using peaks in spectrum:', _spectrum_names)
+        m1.frame.pack(side='top', anchor='w')
+        
+        self.spec_to = m2 = tkutil.option_menu(self.top, 'Assign peaks to GSSs in spectrum:', _spectrum_names)
+        m2.frame.pack(side='top', anchor='w')
+        
+        # matching dimension 1
+        self.match1_from = m3 = tkutil.option_menu(self.top, 'Matching dimension 1: from:', [0, 1, 2]) # TODO do those values need to be strings?
+        m3.frame.pack(side='top', anchor='w')
+        
+        self.match1_to = m4 = tkutil.option_menu(self.top, 'Matching dimension 1: to:', [0, 1, 2])
+        m4.frame.pack(side='top', anchor='w')
+        
+        self.tol1 = tkutil.entry_field(self.top, 'Matching dimension 1: tolerance:', '0.2', 20)
+        self.tol1.frame.pack(side = 'top', anchor = 'w')
+
+        # matching dimension 1
+        self.match2_from = tkutil.option_menu(self.top, 'Matching dimension 2: from:', [0, 1, 2]) # TODO do those values need to be strings?
+        self.match2_from.frame.pack(side='top', anchor='w')
+        
+        self.match2_to = tkutil.option_menu(self.top, 'Matching dimension 2: to:', [0, 1, 2])
+        self.match2_to.frame.pack(side='top', anchor='w')
+        
+        self.tol2 = tkutil.entry_field(self.top, 'Matching dimension 2: tolerance:', '0.2', 20)
+        self.tol2.frame.pack(side = 'top', anchor = 'w')
+
+        # TODO check that the nuclei match
+        # TODO are 2 matching dimensions enough?
+    
+    def execute(self):
+        d1 = [int(self.match1_from.variable.get()), # TODO do I need to use `int` here?
+              int(self.match1_to.variable.get()),
+              float(self.tol1.variable.get())]
+        d1 = [int(self.match2_from.variable.get()), # TODO do I need to use `int` here?
+              int(self.match2_to.variable.get()),
+              float(self.tol2.variable.get())]
+        model.group_peaks_into_gss([d1, d2], 
+                                   self.spec_from.variable.get(),
+                                   self.spec_to.variable.get())
+    
+
+
 class Snapshot_dialog(tkutil.Dialog):
 
     def __init__(self, session):
